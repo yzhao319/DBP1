a4c314915cc8a59dd4595564402b84d6e6940c1f
Yuanwei Zhao <yz4027@columbia.edu>
2021-11-11 10:55:18 -0500

    submit

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -4,6 +4,8 @@ from flask import (
 from werkzeug.exceptions import abort
 
 from flaskr.db import get_db
+from datetime import date
+
 import json
 
 
@@ -36,11 +38,7 @@ def inventory():
 
 @bp.route('/create')
 def create():
-    db = get_db()
-    inventories = db.execute(
-        'SELECT * FROM Stored_in'
-    ).fetchall()
-    return render_template('create.html', inventories=inventories)
+    return render_template('create.html')
 
 
 @bp.route("/search",methods=["POST","GET"])
@@ -66,3 +64,86 @@ def reset():
     myDict = {}
     myDict["res"] = result
     return myDict
+
+@bp.route("/buy",methods=["POST","GET"])
+def buy():
+    sales = "*" if request.form.get("sales") == "" else request.form.get("sales")
+    customer = "*" if request.form.get("customer") == "" else request.form.get("customer")
+    vin = "*" if request.form.get("vin") == "" else request.form.get("vin")
+    error = None
+
+    db = get_db()
+    sales = db.execute(
+        "SELECT e_id FROM Employees WHERE name = '{0}'".format(sales)
+    ).fetchall()
+
+    customer = db.execute(
+        "SELECT c_id FROM customers WHERE name = '{0}'".format(customer)
+    ).fetchall()
+
+    vin = db.execute(
+        "SELECT vin,MSRP FROM Inventory WHERE vin = '{0}'".format(vin)
+    ).fetchall()
+
+    if len(sales) == 0:
+        error = f"Sales name is not valid!"
+
+    if len(customer) == 0:
+        error = f"customer name is not valid!"
+    
+    if len(vin) == 0:
+        error = f"vin is not valid!"
+    
+    in_stock = db.execute(
+        "SELECT * FROM Stored_in WHERE vin = '{0}'".format(vin[0][0])
+    ).fetchall()
+    if len(in_stock) == 0:
+        error = f"the car is out of stock!"
+    
+    discount = db.execute(
+        "SELECT discount_rate FROM Manager WHERE e_id = '{0}'".format(sales[0][0])
+    ).fetchall()
+    discountRate = 1 if len(discount) == 0 else (1-discount[0][0])
+
+    orderNum = db.execute(
+        "SELECT order_number FROM Ord"
+    ).fetchall()[-1][0]+1
+
+    print (discountRate)
+    print (vin[0][1])
+
+    if error is None:
+        db.execute(
+            "INSERT INTO ord(order_number, price, order_date, vin) VALUES ({0},{1},'{2}', '{3}')".format(orderNum, vin[0][1]*discountRate, date.today(), vin[0][0])
+        )
+        db.execute(
+            "INSERT INTO Purchase(c_id, order_number) VALUES ({0},{1})".format(customer[0][0],orderNum)
+        )
+        db.execute(
+            "INSERT INTO Sell(e_id, order_number) VALUES ({0},{1})".format(sales[0][0],orderNum)
+        )
+        return "Order Success!"
+    return error
+
+@bp.route("/sale",methods=["POST","GET"])
+def sale():
+    db = get_db()
+    order = -1 if request.form.get("order") == "" else int(request.form.get("order"))
+    error = None
+
+    print (order)
+    if order == -1:
+        error = f"Order number is  invalid!"
+    
+    if error is None:
+        db.execute(
+            "DELETE FROM Purchase WHERE order_number= {0}".format(order)
+        )
+        db.execute(
+            "DELETE FROM Sell WHERE order_number= {0}".format(order)
+        )
+        db.execute(
+            "DELETE FROM ord WHERE order_number= {0}".format(order)
+        )
+        return "Order Success!"
+    return error
