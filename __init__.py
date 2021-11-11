8d963c98380c85eed0eeb6814cd7aa2bb0b66075
Yuanwei Zhao <yz4027@columbia.edu>
2021-11-11 02:20:49 -0500

    .

--- __init__.py
+++ __init__.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask
 
+import flaskr
+
 
 def create_app(test_config=None):
     # create and configure the app
@@ -11,6 +13,7 @@ def create_app(test_config=None):
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
     app.jinja_env.auto_reload = True
+    
 
 
     if test_config is None:
@@ -26,11 +29,39 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    from . import db
+    import flaskr.db as db
     db.init_app(app)
 
-    from . import blog
+    import flaskr.blog as blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
 
     return app
+
+if __name__ == "__main__":
+    import click
+
+    @click.command()
+    @click.option('--debug', is_flag=True)
+    @click.option('--threaded', is_flag=True)
+    @click.argument('HOST', default='0.0.0.0')
+    @click.argument('PORT', default=8111, type=int)
+
+    def run(debug, threaded, host, port):
+        """
+        This function handles command line parameters.
+        Run the server using:
+
+            python server.py
+
+        Show the help text using:
+
+            python server.py --help
+
+        """
+
+        HOST, PORT = host, port
+        print ("running on %s:%d" % (HOST, PORT))
+        create_app().run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+    
+    run()
